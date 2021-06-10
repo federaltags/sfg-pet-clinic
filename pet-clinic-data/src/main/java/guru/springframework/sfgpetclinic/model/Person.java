1ec0349bce98fbb301a20374ff636895ba4782cd
tom <tom.byttebier@protonmail.com>
2021-06-10 21:38:00 +0200

    147. Spring Pet Clinic - Convert Owner to JPA Entities

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,9 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
 
-    private String lastName;
+    @Column(name = "first_name") //redundant
     private String firstName;
+    @Column(name = "last_name") //redundant
+    private String lastName;
 
     public String getLastName() {
         return lastName;
